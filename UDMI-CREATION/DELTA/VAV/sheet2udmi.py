48bc2fddbb7541053e6e1ea946c2cc1851282c82
Darshan Jain <darshan.jain@secutechautomation.com>
2023-03-11 15:29:24 +0530

    sss

--- UDMI-CREATION/DELTA/VAV/sheet2udmi.py
+++ UDMI-CREATION/DELTA/VAV/sheet2udmi.py
@@ -201,7 +201,7 @@ class UDMISiteModelGenerator:
       if device[self._asset_columns.POINTSET_POINTS] == payload_network_type:
         networks.update({
           payload_localnet_families: {
-            "id": payload_localnet_id
+            "addr": payload_localnet_id
           }
         })
 
